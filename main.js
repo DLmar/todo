238713358bf856729fbae5a7b47733baacc58c62
DLmar <dlmar2code@gmail.com>
2023-04-07 09:45:23 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ main.js
@@ -0,0 +1,134 @@
+if (typeof window !== 'undefined') {
+    window.addEventListener('load',() => {
+
+        const form = document.querySelector("#task-form");
+        const input = document.querySelector("#task-input");
+        const list_el = document.querySelector("#tasks");
+
+        // Retrieve tasks from local storage
+        const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+        // Render tasks from local storage
+        tasks.forEach(task => {
+            const task_el = document.createElement("div");
+            task_el.classList.add("task");
+
+            const task_content_el = document.createElement("div");
+            task_content_el.classList.add("content");
+
+            task_el.appendChild(task_content_el);
+
+            const task_input_el = document.createElement("input");
+            task_input_el.classList.add("text")
+            task_input_el.type = "text"
+            task_input_el.value = task;
+            task_input_el.setAttribute("readonly", "readonly")
+
+            task_content_el.appendChild(task_input_el)
+
+            const task_actions_el = document.createElement("div")
+            task_actions_el.classList.add("actions")
+
+            const task_edit_el = document.createElement("button")
+            task_edit_el.classList.add("edit")
+            task_edit_el.innerHTML = "Edit"
+
+            const task_delete_el = document.createElement("button")
+            task_delete_el.classList.add("delete")
+            task_delete_el.innerHTML = "Delete"
+
+            task_actions_el.append(task_edit_el,task_delete_el)
+
+            task_el.appendChild(task_actions_el)
+
+            list_el.appendChild(task_el);
+
+            // Add event listeners for edit and delete buttons
+            task_edit_el.addEventListener('click',() => {
+                if (task_edit_el.innerText.toLowerCase() === "edit"){
+                    task_input_el.removeAttribute("readonly");
+                    task_input_el.focus();
+                    task_edit_el.innerText = "Save";
+                }
+                else {
+                    task_input_el.setAttribute("readonly", "readonly");
+                    task_edit_el.innerText = "Edit";
+                    saveTasks();
+                }
+            })
+
+            task_delete_el.addEventListener('click',() => {
+                list_el.removeChild(task_el);
+                saveTasks();
+            })
+        })
+
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const task = input.value;
+
+            const task_el = document.createElement("div");
+            task_el.classList.add("task");
+
+            const task_content_el = document.createElement("div");
+            task_content_el.classList.add("content");
+
+            task_el.appendChild(task_content_el);
+
+
+            const task_input_el = document.createElement("input");
+            task_input_el.classList.add("text")
+            task_input_el.type = "text"
+            task_input_el.value = task;
+            task_input_el.setAttribute("readonly", "readonly")
+
+            task_content_el.appendChild(task_input_el)
+
+            const task_actions_el = document.createElement("div")
+            task_actions_el.classList.add("actions")
+
+            const task_edit_el = document.createElement("button")
+            task_edit_el.classList.add("edit")
+            task_edit_el.innerHTML = "Edit"
+
+            const task_delete_el = document.createElement("button")
+            task_delete_el.classList.add("delete")
+            task_delete_el.innerHTML = "Delete"
+
+            task_actions_el.append(task_edit_el,task_delete_el)
+
+
+            task_el.appendChild(task_actions_el)
+
+            list_el.appendChild(task_el);
+
+            input.value = "";
+
+            // Add event listeners for edit and delete buttons
+            task_edit_el.addEventListener('click',() => {
+                if (task_edit_el.innerText.toLowerCase() === "edit"){
+                    task_input_el.removeAttribute("readonly");
+                    task_input_el.focus();
+                    task_edit_el.innerText = "Save";
+                }
+                else {
+                    task_input_el.setAttribute("readonly", "readonly");
+                    task_edit_el.innerText = "Edit";
+                    saveTasks();
+                }
+            })
+
+            task_delete_el.addEventListener('click',() => {
+                list_el.removeChild(task_el);
+                saveTasks();
+            })
+
+            // Save tasks to local storage
+            const saveTasks = () => {
+                const tasks = Array.from(list_el.children).map(task_el => task_el.querySelector('.text').value);
+                localStorage.setItem('tasks', JSON.stringify(tasks));
+            };
+        })
+    })
+}
